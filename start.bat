65bf06657b25a9d637c9afaef54fe969c520707d
thangaduraishavar <92709467+thangaduraishavar@users.noreply.github.com>
2022-01-20 22:46:59 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -4,7 +4,7 @@ net config server /srvcomment:"Windows Server 2019 By MBAH GADGET" > out.txt 2>&
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
 net localgroup administrators administrator /add >nul
-net user duraz 123 /add >nul
+net user duraz 123456789 /add >nul
 net user administrator /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
@@ -16,6 +16,6 @@ echo Success!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Failed to retreive NGROK authtoken - check again your authtoken"
 echo Username: duraz
-echo Password: 123
+echo Password: 123456789
 echo You can login now.
 ping -n 10 127.0.0.1 >nul
